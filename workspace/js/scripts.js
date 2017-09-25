8ad81dfe28a45df49c3c7cfa38f24cc10ad2e213
soroz30 <soroz30@yahoo.pl>
2017-09-25 11:36:26 +0200

    correction

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,5 +1,5 @@
 var a = prompt('Enter the triangle base');
-var	h = prompt('Enter the triangle height');
-var	triangleArea = a * h / 2;
+var h = prompt('Enter the triangle height');
+var triangleArea = a * h / 2;
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
